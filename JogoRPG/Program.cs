4697e36b82c5b5f3df3f00ec9454e3a6bebcf36b
Lincon Class <linconclass@gmail.com>
2022-05-08 23:40:47 -0300

    add project

---
+++ JogoRPG/Program.cs
@@ -0,0 +1,18 @@
+﻿using JogoRPG.src.Entities;
+using System;
+
+namespace JogoRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new("Arus", 23, "Knight");            
+            Wizard wizard = new("Alice", 24, "Mage");
+            
+            Console.WriteLine(arus.ToString() + "\n" + arus.Attack() + "\n");
+            Console.WriteLine(wizard.ToString() + "\n" + wizard.Attack() + "\n" + wizard.Attack(2));
+        }
+
+    }
+}
---
+++ JogoRPG/src/Entities/Wizard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoRPG.src.Entities
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string Name, int Level, string HeroType) : base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack()
+        {
+            return this.Name + ": lançou magia";
+        }
+
+        public string Attack(int Bonus)
+        {
+            return this.Name + ": Lançou magia com bonus de ataque de " + Bonus;
+        }
+    }
+}
